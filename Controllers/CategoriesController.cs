8172b7ed36edf0c1416b0e4e46be93a1f0baf0b6
SmokingTheGoaT <orlando.heaply@gmail.com>
2021-05-24 04:09:07 -0700

    Added domain and models for a Product/Categories REST API

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleNETapp.Controllers
+{
+    [Route("/api/[controller]")]
+    public class CategoriesController : Controller
+    {
+        
+    }
+}
